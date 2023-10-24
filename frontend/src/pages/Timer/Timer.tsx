7708a11a429f52c52d5ae3f331fbc00a4141870a
Vitalii Bilostotskyi <bilostotskyi1991@gmail.com>
2023-10-24 21:25:36 +0300

    refactor: :recycle: started to do Timer

--- frontend/src/pages/Timer/Timer.tsx
+++ frontend/src/pages/Timer/Timer.tsx
@@ -1,7 +1,22 @@
 import React, { FC } from 'react';
+import styles from './Timer.module.scss';
+import { Button } from '../../common/Button';
 
 const Timer: FC = () => {
-	return <div>Timer</div>;
+	return (
+		<div className={styles.timer}>
+			<h2 className={styles.title}>Task title</h2>
+			<h5 className={styles.description}>Task description</h5>
+			<div className='buttonsHolder'>
+				<Button customType='save' buttonType={'button'} children={'Start'} />
+				{/* <Button
+					customType='cancel'
+					buttonType={'button'}
+					children={'Restare'}
+				/> */}
+			</div>
+		</div>
+	);
 };
 
 export default Timer;
